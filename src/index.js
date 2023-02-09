84326f01cceb9c5234de60bfe3601a1ca3da5c63
Liuddmyla <112398546+Liuddmyla@users.noreply.github.com>
2023-02-09 19:18:33 +0200

    api

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 import './css/styles.css';
+import fetchCountries from "./fetchCountries.js";
+import lodashDebounce from 'lodash.debounce';
 
 const DEBOUNCE_DELAY = 300;
+
+const input = document.getElementById('search-box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
+
+// fetchCountries('ukraine').then(console.log);
+
+input.addEventListener('input', lodashDebounce(onInput, 300));
+
+function onInput(e) {
+    e.preventDefault();
+    const value = e.target.value.trim();
+    if (value === '') {
+        return;
+    }
+
+    fetchCountries(value).then((data) => console.log(data));
+}
